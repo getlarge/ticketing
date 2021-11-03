ee386a06af1b0403eb1351899ccea0de28d6b53c
getlarge <ed@getlarge.eu>
2021-11-03 18:31:53 +0100

    feat: create Error classes and handlers

---
+++ apps/auth/src/app/errors/database-connection-error.ts
@@ -0,0 +1,19 @@
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = 'Error connecting to the database';
+
+  constructor() {
+    super('Error connecting to the database');
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
+
+export const isDatabaseConnectionError = (
+  error: Error
+): error is DatabaseConnectionError => error instanceof DatabaseConnectionError;
---
+++ apps/auth/src/app/errors/index.ts
@@ -0,0 +1,4 @@
+export * from './custom-error';
+export * from './database-connection-error';
+export * from './not-found-error';
+export * from './request-validation-error';
