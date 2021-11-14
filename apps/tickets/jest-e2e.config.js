9468e7cfb034757754af805a07e1207e304a88e6
getlarge <ed@getlarge.eu>
2021-11-14 18:44:04 +0100

    test(tickets): create test setup

---
+++ apps/tickets/jest-e2e.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  displayName: 'tickets',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[t]s$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'js', 'node'],
+  collectCoverageFrom: ['./src/**/*.(t|j)s'],
+  coverageDirectory: '../../coverage/apps/tickets-e2e',
+  coverageReporters: ['json'],
+  testMatch: ['**/+(*.)+(e2e-spec|test).+(ts|js)?(x)'],
+};
