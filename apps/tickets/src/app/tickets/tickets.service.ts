240029afdd2a4a9d5c854194eaa95c5d77a37f30
getlarge <ed@getlarge.eu>
2021-11-18 18:27:07 +0100

    feat(tickets): publish ticket created/updated events

--- apps/tickets/src/app/tickets/tickets.service.ts
+++ apps/tickets/src/app/tickets/tickets.service.ts
@@ -1,11 +1,18 @@
 import {
   ForbiddenException,
+  Inject,
   Injectable,
   Logger,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { CreateTicket, Ticket, User } from '@ticketing/shared/models';
+import { Publisher } from '@nestjs-plugins/nestjs-nats-streaming-transport';
+import {
+  Patterns,
+  TicketCreatedEvent,
+  TicketUpdatedEvent,
+} from '@ticketing/microservices/shared/events';
+import * as models from '@ticketing/shared/models';
 import { isEmpty } from 'lodash';
 
 import { Ticket as TicketSchema, TicketModel } from './schemas/ticket.schema';
@@ -15,35 +22,49 @@ export class TicketsService {
   readonly logger = new Logger(TicketsService.name);
 
   constructor(
-    @InjectModel(TicketSchema.name) private ticketModel: TicketModel
+    @InjectModel(TicketSchema.name) private ticketModel: TicketModel,
+    @Inject(Publisher) private publisher: Publisher
   ) {}
 
-  async create(ticket: CreateTicket, currentUser: User): Promise<Ticket> {
+  async create(
+    ticket: models.CreateTicket,
+    currentUser: models.User
+  ): Promise<models.Ticket> {
     const newTicket = await this.ticketModel.create({
       ...ticket,
       userId: currentUser.id,
     });
-    return newTicket.toJSON<Ticket>();
+    const result = newTicket.toJSON<models.Ticket>();
+    this.publisher
+      .emit<TicketCreatedEvent['name'], TicketCreatedEvent['data']>(
+        Patterns.TicketCreated,
+        result
+      )
+      .subscribe({
+        next: (value) =>
+          this.logger.log(`Sent event ${Patterns.TicketCreated} ${value}`),
+      });
+    return result;
   }
 
-  async find(): Promise<Ticket[]> {
+  async find(): Promise<models.Ticket[]> {
     const tickets = (await this.ticketModel.find()) || [];
-    return tickets.map((ticket) => ticket.toJSON<Ticket>());
+    return tickets.map((ticket) => ticket.toJSON<models.Ticket>());
   }
 
-  async findById(id: string): Promise<Ticket> {
+  async findById(id: string): Promise<models.Ticket> {
     const ticket = await this.ticketModel.findOne({ _id: id });
     if (isEmpty(ticket)) {
       throw new NotFoundException(`Ticket ${id} not found`);
     }
-    return ticket.toJSON<Ticket>();
+    return ticket.toJSON<models.Ticket>();
   }
 
   async updateById(
     id: string,
-    update: CreateTicket,
-    currenUser: User
-  ): Promise<Ticket> {
+    update: models.CreateTicket,
+    currenUser: models.User
+  ): Promise<models.Ticket> {
     const ticket = await this.ticketModel.findOne({ _id: id });
     if (isEmpty(ticket)) {
       throw new NotFoundException(`Ticket ${id} not found`);
@@ -54,6 +75,11 @@ export class TicketsService {
 
     ticket.set(update);
     await ticket.save();
-    return ticket.toJSON<Ticket>();
+    const result = ticket.toJSON<models.Ticket>();
+    this.publisher.emit<TicketUpdatedEvent['name'], TicketUpdatedEvent['data']>(
+      Patterns.TicketUpdated,
+      result
+    );
+    return result;
   }
 }
