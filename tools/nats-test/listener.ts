b12b9a09f1dd1aeec49dc1c2a3378890ae91d480
getlarge <ed@getlarge.eu>
2021-11-18 18:20:29 +0100

    chore: create nats manual testing scripts

---
+++ tools/nats-test/listener.ts
@@ -0,0 +1,52 @@
+import * as nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+(async function () {
+  console.clear();
+  const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222',
+    
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    stan.once('connect', () => {
+      console.log('Listener connected to NATS server');
+      resolve();
+    });
+
+    stan.once('error', (err) => {
+      reject(err);
+    });
+  });
+
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('orders-subscriptions');
+
+  // const subscription = stan.subscribe('ticket:create', options);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-queue-group',
+    options
+  );
+  subscription.on('message', (msg: nats.Message) => {
+    console.log(msg.getSubject());
+    const data = msg.getData();
+    if (typeof data === 'string') {
+      console.log(
+        `Received event number ${msg.getSequence()}, with data : ${data}`
+      );
+    }
+    msg.ack();
+  });
+
+  process.on('SIGINT', () => stan.close());
+  process.on('SIGTERM', () => stan.close());
+})();
---
+++ tools/nats-test/publisher.ts
@@ -0,0 +1,33 @@
+import * as nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+(async function () {
+  console.clear();
+  const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222',
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    stan.once('connect', () => {
+      console.log('Publisher connected to NATS server');
+      resolve();
+    });
+
+    stan.once('error', (err) => {
+      reject(err);
+    });
+  });
+
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  const ticket = { id: '123', title: 'concert', price: 20 };
+  stan.publish('ticket:created', JSON.stringify(ticket), (err, guid) => {
+    console.log('event published', { guid });
+  });
+
+  process.on('SIGINT', () => stan.close());
+  process.on('SIGTERM', () => stan.close());
+})();
