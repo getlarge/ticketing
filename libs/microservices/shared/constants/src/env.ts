eaf2abe843fd548d31a6b5bbffb22eb0bf355b0c
getlarge <ed@getlarge.eu>
2021-11-22 18:47:26 +0100

    feat(microservices-shared-constants): add shared cookie options

---
+++ libs/microservices/shared/constants/src/env.ts
@@ -0,0 +1,7 @@
+import { Environment } from '@ticketing/shared/constants';
+
+export const devEnvironments = [
+  Environment.Test,
+  Environment.Development,
+  Environment.DockerDevelopment,
+];
--- libs/microservices/shared/constants/src/requests.ts
+++ libs/microservices/shared/constants/src/requests.ts
@@ -1,4 +1,8 @@
 import { SecuritySchemeObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import { Environment } from '@ticketing/shared/constants';
+import { CookieSerializeOptions } from 'fastify-cookie';
+
+import { devEnvironments } from './env';
 
 export enum SecurityRequirements {
   Session = 'session',
@@ -16,3 +20,14 @@ export const bearerSecurityScheme: SecuritySchemeObject = {
   scheme: 'bearer',
   bearerFormat: 'JWT',
 };
+
+export const getCookieOptions = (
+  environment: Environment
+): CookieSerializeOptions => ({
+  secure: !devEnvironments.includes(environment),
+  signed: false,
+  path: '/',
+  maxAge: 60 * 60,
+});
+
+export const GLOBAL_API_PREFIX = 'api';
