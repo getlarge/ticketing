f9f35f65e17bae03ac9271fcaf1f17d5cf51905d
getlarge <ed@getlarge.eu>
2021-11-29 18:37:24 +0100

    refactor(tickets): integrate GlobalErrorFilter

--- apps/tickets/src/app/orders/orders-ms.controller.ts
+++ apps/tickets/src/app/orders/orders-ms.controller.ts
@@ -9,13 +9,13 @@ import { Ctx, EventPattern, Payload, Transport } from '@nestjs/microservices';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { NatsStreamingContext } from '@nestjs-plugins/nestjs-nats-streaming-transport';
 import { Patterns } from '@ticketing/microservices/shared/events';
-import { NatsStreamErrorFilter } from '@ticketing/microservices/shared/filters';
+import { GlobalErrorFilter } from '@ticketing/microservices/shared/filters';
 import { requestValidationErrorFactory } from '@ticketing/shared/errors';
 import { Order } from '@ticketing/shared/models';
 
 import { TicketsService } from '../tickets/tickets.service';
 
-@UseFilters(NatsStreamErrorFilter)
+@UseFilters(GlobalErrorFilter)
 @Controller()
 export class OrdersMSController {
   readonly logger = new Logger(OrdersMSController.name);
--- apps/tickets/src/app/orders/orders.module.ts
+++ apps/tickets/src/app/orders/orders.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
-import { NatsStreamErrorFilter } from '@ticketing/microservices/shared/filters';
 
 import { TicketsModule } from '../tickets/tickets.module';
 import { OrdersMSController } from './orders-ms.controller';
@@ -8,12 +6,6 @@ import { OrdersMSController } from './orders-ms.controller';
 @Module({
   imports: [TicketsModule],
   controllers: [OrdersMSController],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useExisting: NatsStreamErrorFilter,
-    },
-    NatsStreamErrorFilter,
-  ],
+  providers: [],
 })
 export class OrdersModule {}
