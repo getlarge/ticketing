f87ac22b2a5520b9539dd776946ea0ea0069984c
getlarge <ed@getlarge.eu>
2022-01-20 16:10:56 +0100

    refactor(ng-client): move angular client

--- apps/ng-client/jest.config.js
+++ apps/ng-client/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'angular-client',
+  displayName: 'ng-client',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/angular-client',
+  coverageDirectory: '../../coverage/apps/ng-client',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
