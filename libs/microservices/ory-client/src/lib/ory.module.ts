e202b0fe5a85ec78e86f8a38ee6d6deaa369572f
getlarge <ed@getlarge.eu>
2023-12-07 16:27:26 +0100

    feat(microservices-ory-client): create OryModule

---
+++ libs/microservices/ory-client/src/lib/ory.module.ts
@@ -0,0 +1,75 @@
+import { DynamicModule, Module, Provider, Type } from '@nestjs/common';
+
+import {
+  IOryModuleOptions,
+  OryModuleAsyncOptions,
+  OryModuleOptions,
+  OryModuleOptionsFactory,
+} from './ory.interfaces';
+import { OryService } from './ory.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [OryService],
+})
+export class OryModule {
+  static forRoot(options: IOryModuleOptions): DynamicModule {
+    return {
+      module: OryModule,
+      providers: [{ provide: OryModuleOptions, useValue: options }],
+      exports: [OryModuleOptions],
+    };
+  }
+
+  static forRootAsync(options: OryModuleAsyncOptions): DynamicModule {
+    return {
+      module: OryModule,
+      imports: options.imports ?? [],
+      providers: this.createAsyncProviders(options),
+      exports: [OryModuleOptions],
+    };
+  }
+
+  private static createAsyncProviders(
+    options: OryModuleAsyncOptions
+  ): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return [this.createAsyncOptionsProvider(options)];
+    }
+    if (options.useClass) {
+      return [
+        this.createAsyncOptionsProvider(options),
+        {
+          provide: options.useClass,
+          useClass: options.useClass,
+        },
+      ];
+    }
+    throw new Error('Invalid OryModuleAsyncOptions');
+  }
+
+  private static createAsyncOptionsProvider(
+    options: OryModuleAsyncOptions
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: OryModuleOptions,
+        useFactory: options.useFactory,
+        inject: options.inject ?? [],
+      };
+    }
+    if (!options.useExisting && !options.useClass) {
+      throw new Error('Invalid OryModuleAsyncOptions');
+    }
+    return {
+      provide: OryModuleOptions,
+      useFactory: async (optionsFactory: OryModuleOptionsFactory) =>
+        await optionsFactory.createOryOptions(),
+      inject: [
+        (options.useExisting ??
+          options.useClass) as Type<OryModuleOptionsFactory>,
+      ],
+    };
+  }
+}
