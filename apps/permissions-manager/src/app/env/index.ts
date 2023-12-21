365f150d11061e0947536ba01b5cca244d8f78ef
getlarge <ed@getlarge.eu>
2023-12-21 14:30:02 +0100

    fix(permissions-manager): remove APP_VERSION

--- apps/permissions-manager/src/app/env/index.ts
+++ apps/permissions-manager/src/app/env/index.ts
@@ -1,22 +1,18 @@
 import { ConfigService } from '@nestjs/config';
 import { OryKetoEnvironmentVariables } from '@ticketing/microservices/shared/env';
 import { Exclude } from 'class-transformer';
-import { readFileSync } from 'node:fs';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { Mixin } from 'ts-mixer';
 
 export type AppConfigService = ConfigService<EnvironmentVariables, true>;
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const pkgPath = join(__dirname, '..', '..', '..', '..', '..', 'package.json');
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// const pkgPath = join(__dirname, '..', '..', '..', '..', '..', 'package.json');
 
 export class EnvironmentVariables extends Mixin(OryKetoEnvironmentVariables) {
   @Exclude()
-  private pkg: { [key: string]: unknown; name?: string; version?: string } =
-    JSON.parse(readFileSync(pkgPath, 'utf8'));
-
+  // private pkg: { [key: string]: unknown; name?: string; version?: string } =
+  //   JSON.parse(readFileSync(pkgPath, 'utf8'));
   APP_NAME?: string = 'payments';
 
-  APP_VERSION?: string = this.pkg?.version || '0.0.1';
+  APP_VERSION?: string = '0.0.1';
 }
