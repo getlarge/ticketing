d0f4d81eaa492d3adb78e0dce29bf240e57e5c6b
getlarge <ed@getlarge.eu>
2024-01-05 14:21:41 +0100

    feat(moderation): add extra env variables support

--- apps/moderation/src/app/env/index.ts
+++ apps/moderation/src/app/env/index.ts
@@ -1,16 +1,42 @@
 import { ConfigService } from '@nestjs/config';
-import { BaseEnvironmentVariables } from '@ticketing/microservices/shared/env';
+import {
+  BaseEnvironmentVariables,
+  MongoEnvironmentVariables,
+  OryHydraEnvironmentVariables,
+  OryKetoEnvironmentVariables,
+  OryKratosEnvironmentVariables,
+  RedisEnvironmentVariables,
+  RmqEnvironmentVariables,
+} from '@ticketing/microservices/shared/env';
 import { Exclude } from 'class-transformer';
 import { readFileSync } from 'node:fs';
-import { join } from 'node:path';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { Mixin } from 'ts-mixer';
 
 export type AppConfigService = ConfigService<EnvironmentVariables, true>;
 
-export class EnvironmentVariables extends Mixin(BaseEnvironmentVariables) {
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pkgPath = join(__dirname, '..', '..', '..', '..', '..', 'package.json');
+
+class OryEnvironmentVariables extends Mixin(
+  OryHydraEnvironmentVariables,
+  OryKetoEnvironmentVariables,
+  OryKratosEnvironmentVariables,
+) { }
+
+export class EnvironmentVariables extends Mixin(
+  BaseEnvironmentVariables,
+  MongoEnvironmentVariables,
+  RedisEnvironmentVariables,
+  RmqEnvironmentVariables,
+  OryEnvironmentVariables,
+) {
   @Exclude()
   private pkg: { [key: string]: unknown; name?: string; version?: string } =
-    JSON.parse(readFileSync(join(process.cwd(), 'package.json'), 'utf8'));
+    JSON.parse(
+      readFileSync(pkgPath, 'utf8'),
+    );
 
   APP_NAME?: string = 'moderation';
 
