e951e7aa621dd5155c20bcfce8cb65fc4a56b830
getlarge <ed@getlarge.eu>
2024-02-29 12:09:22 +0100

    feat(shared-errors): implement generic errors

---
+++ libs/shared/errors/src/generic-error.ts
@@ -0,0 +1,38 @@
+import { CustomError } from './custom-error';
+import { ErrorResponse } from './error-response';
+
+export abstract class GenericError extends CustomError {
+  abstract readonly name: string;
+
+  constructor(
+    message: string,
+    public readonly statusCode: number,
+    public readonly path: string = '',
+    public readonly details: Record<string, unknown> = {},
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, GenericError.prototype);
+  }
+
+  getDetails(): Record<string, unknown> {
+    return this.details;
+  }
+
+  serializeErrors(): { message: string }[] {
+    return [{ message: this.message }];
+  }
+
+  toErrorResponse(): ErrorResponse {
+    return new ErrorResponse({
+      name: this.name,
+      statusCode: this.statusCode,
+      errors: this.serializeErrors(),
+      details: this.getDetails(),
+      path: this.path,
+    });
+  }
+}
+
+export function isGenericError(error: unknown): error is GenericError {
+  return error instanceof GenericError;
+}
---
+++ libs/shared/errors/src/retriable-error.ts
@@ -0,0 +1,28 @@
+import { GenericError } from './generic-error';
+
+/**
+ * @description Retriable errors are errors that can be resolved by retrying the request on the producer side.
+ * @example
+ * - A request to a service that is currently unavailable.
+ * - A request to a service that is currently overloaded.
+ * - A request that is being throttled.
+ * - A request that is being rate limited.
+ * - A request that is being blocked by a firewall.
+ */
+export class RetriableError extends GenericError {
+  public readonly name = 'RetriableError';
+
+  constructor(
+    message: string,
+    statusCode: number,
+    path: string,
+    details: Record<string, unknown> = {},
+  ) {
+    super(message, statusCode, path, details);
+    Object.setPrototypeOf(this, RetriableError.prototype);
+  }
+}
+
+export function isRetriableError(error: unknown): error is RetriableError {
+  return error instanceof RetriableError;
+}
