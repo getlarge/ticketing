123bcc1737ccd934e44550da5cbe7f00436a2e4d
getlarge <ed@getlarge.eu>
2024-04-09 20:13:52 +0200

    feat(auth): create initial ClientsModule

---
+++ apps/auth/src/app/clients/clients.controller.ts
@@ -0,0 +1,114 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { SecurityRequirements } from '@ticketing/microservices/shared/constants';
+import {
+  CurrentClient,
+  CurrentUser,
+} from '@ticketing/microservices/shared/decorators';
+import {
+  OryActionAuthGuard,
+  OryAuthenticationGuard,
+  OryOAuth2AuthenticationGuard,
+} from '@ticketing/microservices/shared/guards';
+import { Actions, Resources } from '@ticketing/shared/constants';
+import { requestValidationErrorFactory } from '@ticketing/shared/errors';
+
+import { User } from '../users/models';
+import { ClientsService } from './clients.service';
+import {
+  Client,
+  ClientDto,
+  CreateClientDto,
+  OryOAuth2WebhookPayloadDto,
+  OryOAuth2WebhookResponseDto,
+} from './models';
+
+@Controller(Resources.CLIENTS)
+@ApiTags(Resources.CLIENTS)
+export class ClientsController {
+  constructor(private readonly clientsService: ClientsService) {}
+
+  @UseGuards(OryAuthenticationGuard())
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: requestValidationErrorFactory,
+      forbidUnknownValues: true,
+    }),
+  )
+  @ApiOperation({
+    description: 'Register a new client',
+    summary: `Register a new client - Scope : ${Resources.CLIENTS}:${Actions.CREATE_ONE}`,
+  })
+  @ApiBody({ type: CreateClientDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Client created',
+    type: ClientDto,
+  })
+  @Post('')
+  @HttpCode(HttpStatus.CREATED)
+  create(
+    @CurrentUser() user: User,
+    @Body() body: CreateClientDto,
+  ): Promise<ClientDto> {
+    return this.clientsService.create(body, user);
+  }
+
+  @UseGuards(OryActionAuthGuard)
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: requestValidationErrorFactory,
+      forbidUnknownValues: true,
+    }),
+  )
+  @ApiOperation({
+    description: 'Triggered when a client request an OAuth2 token',
+  })
+  @ApiBody({ type: OryOAuth2WebhookPayloadDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Token session update',
+    type: OryOAuth2WebhookResponseDto,
+  })
+  @Post('on-token-request')
+  @HttpCode(HttpStatus.OK)
+  onSignUp(
+    @Body() body: OryOAuth2WebhookPayloadDto,
+  ): Promise<OryOAuth2WebhookResponseDto> {
+    return this.clientsService.onTokenRequest(body);
+  }
+
+  @UseGuards(OryOAuth2AuthenticationGuard())
+  @ApiOperation({
+    description: 'Get details about currently authenticated client',
+    summary: `Get current user - Scope : ${Resources.CLIENTS}:${Actions.READ_ONE}`,
+  })
+  @ApiBearerAuth(SecurityRequirements.Bearer)
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Current client authenticated',
+    type: ClientDto,
+  })
+  @Get('current-client')
+  getCurrentClient(@CurrentClient() client: Client): Client {
+    return client;
+  }
+}
---
+++ libs/microservices/shared/decorators/src/current-client.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { CURRENT_CLIENT_KEY } from '@ticketing/shared/constants';
+import { FastifyRequest } from 'fastify';
+
+export const CurrentClient = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<FastifyRequest>();
+    return request[CURRENT_CLIENT_KEY];
+  }
+);
