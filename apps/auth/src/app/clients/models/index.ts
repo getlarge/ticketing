96fdd619cd285668f8155dd3bf04b8abfe18ae97
getlarge <ed@getlarge.eu>
2024-04-09 20:10:02 +0200

    feat(auth): create models for Clients module

---
+++ apps/auth/src/app/clients/models/index.ts
@@ -0,0 +1,4 @@
+export * from './client.dto';
+export * from './create-client.dto';
+export * from './ory-token.dto';
+export { Client } from '@ticketing/shared/models';
---
+++ apps/auth/src/app/clients/schemas/client.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Model, ObjectId, Schema as MongooseSchema } from 'mongoose';
+
+import { User } from '../../users/schemas';
+import { Client as ClientAttrs } from '../models';
+
+@Schema({
+  toJSON: {
+    transform(doc: ClientDocument, ret: ClientAttrs & { _id: ObjectId }) {
+      ret.id = doc._id.toString();
+      ret.userId = doc.user._id.toString();
+      const { _id, ...rest } = ret;
+      return rest;
+    },
+  },
+})
+export class Client extends ClientAttrs {
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: 'User',
+  })
+  user: User & Document;
+
+  @Prop({ type: String, required: true, unique: true, index: true })
+  declare clientId: string;
+}
+
+export type ClientDocument = Client & Document;
+
+export const ClientSchema = SchemaFactory.createForClass(Client);
+
+export interface ClientModel extends Model<ClientDocument> {
+  build(attr: ClientAttrs): ClientDocument;
+}
